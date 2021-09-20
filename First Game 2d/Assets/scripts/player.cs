3c37f8f8c308ef7c1b0e6bda289fa9b4ae7022b7
RogerioMattos <rogerio.mattos@stoom.com.br>
2021-09-20 18:04:37 -0300

    aula 4: movimentando na horizontal

---
+++ First Game 2d/Assets/scripts/player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Move();
+    }
+
+    void Move()
+    {
+        Vector3 moviment = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += moviment * Time.deltaTime * speed;
+    }
+}
