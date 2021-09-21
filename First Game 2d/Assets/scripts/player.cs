ef7543d287af4c2011fdd68912e1265614dadc73
RogerioMattos <rogerio.mattos@stoom.com.br>
2021-09-21 10:22:06 -0300

    Aula 4: fazendo o pulo do personagem

--- First Game 2d/Assets/scripts/player.cs
+++ First Game 2d/Assets/scripts/player.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
+    private Rigidbody2D rig;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rig = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         this.Move();
+        this.Jump();
     }
 
     void Move()
     {
         Vector3 moviment = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += moviment * Time.deltaTime * speed;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump")){
+            rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
     }
 }
